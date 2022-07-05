aa9e790b2d0435bd62cf5da37d145af46798fa7d
nichollskc <nichollskc@gmail.com>
2022-07-05 16:57:44 +0100

    Add extra simulation plots

--- scripts/plot_results.R
+++ scripts/plot_results.R
@@ -3,8 +3,8 @@ library(ggplot2)
 
 std <- function(x) sd(x)/sqrt(length(x))
 
-simplex_results = read.csv("simplex_results/full_results.csv") #snakemake@input[[1]])
-#simplex_results = read.csv(snakemake@input[[1]])
+#simplex_results = read.csv("old_mclust_simplex_results/full_results.csv") #snakemake@input[[1]])
+simplex_results = read.csv(snakemake@input[[1]])
 cbbPalette = c("#0072B2", "#D55E00", "#009E73", "#E69F00", "#00FFFF")
 
 summary_df = simplex_results %>%
@@ -20,6 +20,7 @@ summary_df = simplex_results %>%
          method = recode(method, DPMUnc_novar = "DPMZeroUnc"),
          U = noise_factor,
          N = var_latents)
+
 print(table(summary_df$nruns))
 g = ggplot(summary_df, aes(colour=method, y=ari_mean, x=U)) +
   geom_line() +
@@ -34,4 +35,34 @@ g = ggplot(summary_df, aes(colour=method, y=ari_mean, x=U)) +
   scale_x_continuous(breaks = unique(summary_df$U)) +
   facet_grid(" N ~ data", labeller = label_both)
 
-ggsave(snakemake@output[[1]], g, width=6, height=8, units="in")
+ggsave(snakemake@output[["ari"]], g, width=6, height=8, units="in")
+
+g = ggplot(summary_df, aes(colour=method, y=estimated_k_mean, x=U)) +
+  geom_line() +
+  scale_colour_manual(values=cbbPalette) +
+  labs(y="Estimated K",
+       colour="Method") +
+  theme(text=element_text(size=15),
+        legend.position = "bottom") +
+  scale_x_continuous(breaks = unique(summary_df$U)) +
+  facet_grid(" N ~ data", labeller = label_both)
+
+ggsave(snakemake@output[["K_line"]], g, width=6, height=8, units="in")
+
+tidied_results = simplex_results %>%
+  filter(d == 2, inferred_K, data == "x") %>%
+  mutate(data = recode(data, x = 'observed', z = 'latent'),
+         method = recode(method, DPMUnc_novar = "DPMZeroUnc"),
+         U = noise_factor,
+         N = var_latents)
+
+g = ggplot(tidied_results, aes(fill=method, x=estimated_k)) +
+  geom_histogram() +
+  scale_fill_manual(values=cbbPalette) +
+  scale_x_continuous(breaks = 1:8) +
+  labs(x="Estimated K") +
+  theme(text=element_text(size=15),
+        legend.position = "bottom") +
+  facet_grid("N ~ method", labeller = label_both)
+
+ggsave(snakemake@output[["K_hist"]], g, width=10, height=8, units="in")
