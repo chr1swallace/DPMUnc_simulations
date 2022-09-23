7f9ccd73ec3b6f2186f644dfdd3d32811d6dfb5d
nichollskc <nichollskc@gmail.com>
2022-09-23 17:16:11 +0100

    Add additional simplex_summary plot with latents in main part of graph

--- scripts/plot_results.R
+++ scripts/plot_results.R
@@ -3,7 +3,7 @@ library(ggplot2)
 
 std <- function(x) sd(x)/sqrt(length(x))
 
-#simplex_results = read.csv("old_mclust_simplex_results/full_results.csv") #snakemake@input[[1]])
+#simplex_results = read.csv("simplex_results/full_results.csv") #snakemake@input[[1]])
 simplex_results = read.csv(snakemake@input[[1]])
 cbbPalette = c("#0072B2", "#D55E00", "#009E73", "#E69F00", "#00FFFF")
 
@@ -36,6 +36,32 @@ g = ggplot(summary_df, aes(colour=method, y=ari_mean, x=U)) +
   facet_grid(" N ~ data", labeller = label_both)
 
 ggsave(snakemake@output[["ari"]], g, width=6, height=8, units="in")
+
+# Filter to only observed data, and one set of the latent variables, which we
+#   will treat as U=0.
+summary_df_nolatent = summary_df %>%
+  filter(data == "observed" | U == 5)
+# Set U = 0 for the latent rows
+summary_df_nolatent[summary_df_nolatent$data == "latent", ]$U= 0
+# Add rows for DPMUnc as well as DPMZeroUnc
+extra_rows = summary_df_nolatent %>%
+  filter(data == "latent", method == "DPMZeroUnc") %>%
+  mutate(method = "DPMUnc")
+summary_df_nolatent = rbind(summary_df_nolatent, extra_rows)
+g = ggplot(summary_df_nolatent, aes(colour=method, y=ari_mean, x=U)) +
+  geom_line() +
+  scale_colour_manual(values=cbbPalette) +
+  geom_errorbar(aes(ymin=ari_mean - ari_se,
+                    ymax=ari_mean + ari_se),
+                width=0.2) +
+  labs(y="Mean accuracy of clustering (ARI)",
+       colour="Method") +
+  theme(text=element_text(size=15),
+        legend.position = "bottom") +
+  scale_x_continuous(breaks = unique(summary_df_nolatent$U)) +
+  facet_grid(" N ~ .", labeller = label_both)
+
+ggsave(snakemake@output[["ari_nolatent"]], g, width=6, height=8, units="in")
 
 g = ggplot(summary_df, aes(colour=method, y=estimated_k_mean, x=U)) +
   geom_line() +
